1b8d4575603867bb0f53353a1fd2b442968150a3
Kihansi95 <ndh3395@gmail.com>
2018-05-26 16:07:54 +0200

    update missing data api

---
+++ api/controllers/DataController.js
@@ -0,0 +1,80 @@
+/**
+ * DataController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+	
+	create : async (req, res) => {
+		
+		try {
+			
+			let data = await Data.create(req.body).fetch();
+			
+			return res.json(data);
+			
+		} catch (err) {
+			sails.log.error(err);
+			return res.status(500).json(err);
+		}
+		
+	},
+	
+	get: async (req, res) => {
+		
+		try {
+			
+			let data = await Data.findOne(req.param('id'));
+			return res.status(200).json(data);
+			
+		} catch (err) {
+			
+			sails.log.error(err.raw);
+			return res.status(500).json(err);
+			
+		}
+		
+	},
+	
+	destroy: async(req, res) => {
+		try {
+			await Data.destroy({id: req.param('id')});
+			return res.ok();
+		} catch (err) {
+			sails.log.error(err.raw);
+			return res.status(500).json(err);
+		}
+		
+		
+	},
+	
+	getAll: async(req, res) => {
+		
+		try {
+			let datas = await Data.find();
+			return res.status(200).json(datas);
+		} catch (err) {
+			
+			sails.log.error(err.raw);
+			return res.status(500).json(err);
+		}
+		
+	},
+	
+	subscribe: async(req, res) => {
+		if(!req.isSocket) {
+			return res.badRequest();
+		}
+		
+		sails.sockets.join(req.socket, 'data', (err) => {
+			if(err) {
+				return res.serverError(err);
+			}
+		});
+		
+		return res.ok();
+	}
+};
+
---
+++ api/models/Data.js
@@ -0,0 +1,44 @@
+/**
+ * Data.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+	
+	attributes: {
+		
+		//  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+		//  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+		//  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+		icon: {
+			type: 'string'
+		},
+		
+		device : {
+			type: 'string'
+		},
+		
+		content: {
+			type: 'string'
+		}
+		
+		//  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+		//  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+		//  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+		
+		
+		//  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+		//  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+		//  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+		
+	},
+	
+	afterCreate: function(data, next) {
+		sails.sockets.broadcast('data', 'new_entry', data);
+		return next();
+	}
+	
+};
+
